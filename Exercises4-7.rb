fd7acc9f2fd5d69f3f299d2b0f0ff2b4b3f9c736
yalasti <yvettecodes@gmail.com>
2015-01-11 21:34:40 -0500

    added exercises

---
+++ Exercises4-7.rb
@@ -0,0 +1,15 @@
+#Exercise 4
+my_array = [1,2,3,4,5,6,,7,8,9,10]
+my_array.push(11)
+my_array.unshift(0)
+
+#Exercise 5
+my_array.delete(11) #or can use my_array.pop
+my_array.push(3)
+
+#Exercise 6
+my_array.uniq
+
+#Exercise 7
+# A Hash is a collection of unique keys and their values. Arrays are ordered, integer-indexed collections of any object.
+
